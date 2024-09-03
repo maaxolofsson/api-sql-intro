b15c64004494a712f8cec46e5e43a3fad1445cb7
Max V Olofsson <max.olofsson@se.experis.com>
2024-09-03 12:58:02 +0200

    Extension 2 done

--- sql/extension/extension.sql
+++ sql/extension/extension.sql
@@ -9,3 +9,53 @@ SELECT COUNT(title) FROM films;
 
 -- Return the average film rating by genre
 SELECT AVG(score), genre FROM films GROUP BY genre;
+
+-- Extension 2
+-- Create directors table
+CREATE TABLE directors (
+    id serial primary key,
+    name text not null
+);
+
+-- Recreate films table, add directorId column
+CREATE TABLE films (
+    id serial primary key,
+    title text not null,
+    genre text not null,
+    release_year int not null,
+    score int not null,
+    directorId int references directors(id),
+    unique(title)
+);
+
+-- Insert a few directors
+INSERT INTO directors 
+    (name)
+VALUES
+    ('Max Olofsson'),
+    ('Sven Larsson'),
+    ('Dir Directorson'),
+    ('Test testsson'),
+    ('Best Directorsson');
+
+-- Re-insert your films data
+INSERT INTO films 
+    (title, genre, release_year, score, directorId)
+VALUES
+    ('The Shawshank Redemption', 'Drama', 1994, 9, 1),
+    ('The Godfather', 'Crime', 1972, 9, 1),
+    ('The Dark Knight', 'Action', 2008, 9, 2),
+    ('Alien', 'SciFi', 1979, 9, 2),
+    ('Total Recall', 'SciFi', 1990, 8, 2),
+    ('The Matrix', 'SciFi', 1999, 8, 3),
+    ('The Matrix Resurrections', 'SciFi', 2021, 5, 4),
+    ('The Matrix Reloaded', 'SciFi', 2003, 6, 2),
+    ('The Hunt for Red October', 'Thriller', 1990, 7, 2),
+    ('Misery', 'Thriller', 1990, 7, 1),
+    ('The Power Of The Dog', 'Western', 2021, 6, 3),
+    ('Hell or High Water', 'Western', 2016, 8, 3),
+    ('The Good the Bad and the Ugly', 'Western', 1966, 9, 2),
+    ('Unforgiven', 'Western', 1992, 7, 4);
+
+-- Using an SQL JOIN
+SELECT title, directors.name FROM films JOIN directors ON films.directorId = directors.id;
